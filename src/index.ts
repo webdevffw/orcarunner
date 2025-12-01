a1a5e05368a775e4f44a95a0eded569388eea46b
mohammad-alfatih <alfaitha@gmail.com>
2025-12-01 17:22:07 +0400

    add library access to globalState for running jobs

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { ActionController } from "./controllers/action";
 export { IEventAction, IWorkerPhase } from "./models/interfaces";
 export { runScript } from './utils/scripting';
+export { globalState } from "./state/jobs";
